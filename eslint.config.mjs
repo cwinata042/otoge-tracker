38394722dd0b350dd910b1dba49a1480894d9e0f
Chelsea Winata <cwinata042@gmail.com>
2025-08-27 23:34:03 -0400

    fixed eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,8 +14,8 @@ const eslintConfig = [
     extends: ['next/core-web-vitals', 'next/typescript'],
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
-      'no-unused-vars': off,
-      '@typescript-eslint/no-unused-vars': off,
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
     },
   }),
 ]
