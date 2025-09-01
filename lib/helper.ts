7334543428c09b402ce92ca3d07e907846f40748
Chelsea Winata <cwinata042@gmail.com>
2025-09-01 19:07:25 -0400

    fix build error

--- lib/helper.ts
+++ lib/helper.ts
@@ -1,6 +1,6 @@
 export function isValidLink(link: string | null) {
   try {
-    let url = link ? new URL(link) : null
+    const url = link ? new URL(link) : null
 
     return url?.protocol === 'http:' || url?.protocol === 'https:'
   } catch {
